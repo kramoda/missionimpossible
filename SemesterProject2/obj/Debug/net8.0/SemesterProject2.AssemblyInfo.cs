566de9769f9e72a11279708264213f6640b7392f
Cynder <cynder877@gmail.com>
2024-04-07 17:45:20 +0200

    Some small file changes

--- SemesterProject2/obj/Debug/net8.0/SemesterProject2.AssemblyInfo.cs
+++ SemesterProject2/obj/Debug/net8.0/SemesterProject2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SemesterProject2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b49237c5b9bbb57725e08da7cc942b6d87b652bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61222d12917d7f28d988f037df4d6aade7374c75")]
 [assembly: System.Reflection.AssemblyProductAttribute("SemesterProject2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SemesterProject2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
