00224e7daa56cf5074826d77cf5fb69b0a9bc13d
Cynder <cynder877@gmail.com>
2024-04-08 21:21:37 +0200

    Added the heating unit menu alongside name and image of the units

---
+++ SemesterProject2/Views/HeatingUnitsView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace SemesterProject2.Views;
+
+public partial class HeatingUnitsView : UserControl
+{
+    public HeatingUnitsView()
+    {
+        InitializeComponent();
+    }
+}
