60adaf7cae23634561b09966b4b9e311a032cf89
Cynder <cynder877@gmail.com>
2024-04-08 17:48:05 +0200

    Changed the button set up for Main menu

--- SemesterProject2/obj/Debug/net8.0/SemesterProject2.AssemblyInfo.cs
+++ SemesterProject2/obj/Debug/net8.0/SemesterProject2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SemesterProject2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+566de9769f9e72a11279708264213f6640b7392f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf7068cc596afc1aa18af5189bb30f640688b0da")]
 [assembly: System.Reflection.AssemblyProductAttribute("SemesterProject2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SemesterProject2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
