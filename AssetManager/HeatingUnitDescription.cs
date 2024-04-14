35ca9aec6bb2bf016363a0a7c7a9e877dc4ceab4
Evved <149811826+Evved@users.noreply.github.com>
2024-04-14 11:44:40 +0200

    commit

---
+++ AssetManager/HeatingUnitDescription.cs
@@ -0,0 +1,48 @@
+using System;
+
+public class HeatingUnitDescription
+{
+    public string NameOfUnit { get; set; }
+    public string Description { get; set; }
+
+    public HeatingUnitDescription(string name, string description)
+    {
+        NameOfUnit = name;
+        Description = description;
+    }
+
+    public void DisplayDescription()
+    {
+        Console.WriteLine($"Name: {NameOfUnit}");
+        Console.WriteLine($"Description: {Description}");
+    }
+}
+
+public class GasBoiler : HeatingUnitDescription
+{
+    public GasBoiler() : base("GB", "Max heat: 5.0 MW\nGas consumption: 1.1 MWh(gas) / MWh(th)")
+    {
+    }
+}
+
+public class OilBoiler : HeatingUnitDescription
+{
+    public OilBoiler() : base("OB", "Max heat: 4.0 MW\nGas consumption: 1.2 MWh(oil) / MWh(th)")
+    {
+    }
+}
+
+public class GasMotor : HeatingUnitDescription
+{
+    public GasMotor() : base("GM", "Max heat: 3.6 MW\nGas consumption: 1.9 MWh(gas) / MWh(th)")
+    {
+    }
+}
+
+public class ElectricBoiler : HeatingUnitDescription
+{
+    public ElectricBoiler() : base("EK", "Max heat: 8.0 MW")
+    {
+    }
+}
+
