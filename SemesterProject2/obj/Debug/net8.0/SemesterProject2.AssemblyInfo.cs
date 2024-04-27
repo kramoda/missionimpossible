12499f7f70a27bb62e8257aa6193e962fa110eda
Cynder <cynder877@gmail.com>
2024-04-27 12:39:46 +0200

    change to gitignore folder to potentially fix merge conflict

--- SemesterProject2/obj/Debug/net8.0/SemesterProject2.AssemblyInfo.cs
+++ SemesterProject2/obj/Debug/net8.0/SemesterProject2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SemesterProject2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f918285b2c5bd4c1db11de82111186403ea0fe9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd193a42ab537318658536deda8a9199c2c26f21")]
 [assembly: System.Reflection.AssemblyProductAttribute("SemesterProject2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SemesterProject2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
